b3e77fa9ce0e2030ad25aa961288d8e9b369f790
Atahan Ekici <atahanekici@hotmail.com>
2023-02-25 01:09:18 +0300

    Dumping data to new Repo

---
+++ Assets/Scripts/ButtonResizer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class ButtonResizer : MonoBehaviour
+{
+    private bool Expend = true;
+
+    [SerializeField] private float Animation_Speed = 200f;
+    [SerializeField] private Vector3 scale;
+    [SerializeField] private Vector3 rate = new Vector3(0.01f, 0.01f, 0.01f);
+    [SerializeField] private float MAX_Scale = 2.5f;
+    [SerializeField] private float MIN_Scale = 1.3f;
+
+    private void Start()
+    {
+        scale = gameObject.transform.localScale;
+    }
+    private void Update()
+    {
+            ScaleAnimation(Time.unscaledDeltaTime);
+    }
+    private void ScaleAnimation(float a)
+    {
+        if(Expend == true)
+        {
+            scale = gameObject.transform.localScale += rate * a * Animation_Speed;
+
+            float X = scale.x;
+
+            if (X >= MAX_Scale)
+            {
+                Expend = false;
+            }
+        }
+        else if(Expend == false)
+        {
+            scale = gameObject.transform.localScale -= rate * a * Animation_Speed;
+
+            float X = scale.x;
+
+            if (X <= MIN_Scale)
+            {
+                Expend = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private PlayerController player;
+    [SerializeField] private Canvas gameOver_Canvas;
+    [SerializeField] private Canvas Overlay_Canvas;
+    [SerializeField] private Canvas Play_Canvas;
+
+    private AsyncOperation asyncLoadLevel;
+    private static GameManager _instance;
+
+    private static GameManager Instance
+    {
+        get { return _instance; }
+    }
+    void Awake()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    void Start()
+    {
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
+
+        if(PlayerPrefs.GetInt("Play_Button") == 1)
+        {
+            Time.timeScale = 0;
+            Play_Canvas.gameObject.SetActive(true);
+            gameOver_Canvas.gameObject.SetActive(false);
+            Overlay_Canvas.gameObject.SetActive(false);
+            PlayerPrefs.SetInt("Play_Button", 0);
+        }
+        else
+        {
+            Overlay_Canvas.gameObject.SetActive(true);
+            gameOver_Canvas.gameObject.SetActive(false);
+            Play_Canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void Start_Game()
+    {
+        Time.timeScale = 1;
+        Play_Canvas.gameObject.SetActive(false);
+        Overlay_Canvas.gameObject.SetActive(true);
+    }
+    private void CheckPosition(float distance)
+    {
+        if (player != null)
+        {
+            if (player.transform.position.y <= distance)
+            {
+                GameOver();
+            }
+        }
+    }
+    public static void Quit_Game()
+    {
+        Application.Quit();
+    }
+    public void GameOver()
+    {
+        player.gameObject.SetActive(false);
+        gameOver_Canvas.gameObject.SetActive(true);
+        Overlay_Canvas.gameObject.SetActive(false);
+    }
+    public void POR() // Pause Or Resume Function //
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Pause_Or_Resume();
+        }
+    }
+    private void Pause_Or_Resume()
+    {
+        if(Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        } 
+    }
+    private IEnumerator Load_Level()
+    {
+        asyncLoadLevel = SceneManager.LoadSceneAsync("1", LoadSceneMode.Single);
+
+        while (!asyncLoadLevel.isDone)
+        {
+            yield return null;
+        }
+    }
+    public static void V_Sync()
+    {
+        if(QualitySettings.vSyncCount == 1)
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+    }
+    public void Force_Frame_Rate(int given_frame_rate)
+    {
+        Application.targetFrameRate = given_frame_rate;
+    }
+    public void Restart_The_Game()
+    {
+        StartCoroutine(Load_Level());
+    }
+}
